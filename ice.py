f7a33cd30915d9bd2350717227c014ee98e5dee1
Derek Ellis <derekellis170@gmail.com>
2022-03-16 11:35:44 -0400

    Map export test (#2)
    * Add basic map export for testing
    
    * Remove type hints
    
    * Add workaround for qgis import
    
    * Add files via upload
    
    function to clip ice chart to study region
    
    Co-authored-by: sadafnahyaan <98900295+sadafnahyaan@users.noreply.github.com>

--- ice.py
+++ ice.py
@@ -1,5 +1,75 @@
+# noinspection PyUnresolvedReferences
+import patch_env
+from qgis.PyQt.QtXml import QDomDocument
+from qgis.core import *
+
+
+def config_qgis():
+    """
+    Initialize an instance of QGIS
+
+    :return: Reference to the QGIS instance, to be closed later
+    """
+    qgs = QgsApplication([], False)
+
+    print("Initializing QGIS")
+    qgs.initQgis()
+    return qgs
+
+
+def qgis_load_layout(path):
+    """
+    Loads a QgsPrintLayout from a template file.
+
+    :param path: Path to a .qpt file to load
+    :return: A QgsPrintLayout instance loaded from the file
+    """
+    document = QDomDocument()
+    with open(path) as f:
+        document.setContent(f.read())
+
+    layout = QgsPrintLayout(QgsProject.instance())
+    layout.loadFromTemplate(document, QgsReadWriteContext())
+
+    return layout
+
+
+def export_map_test(title, data_path, output_path):
+    """
+    Exports a map based on the test layout template
+
+    :param title: Title to be displayed in the exported map
+    :param data_path: Path to the data to be loaded into the map
+    :param output_path: Path to save the exported map to
+    """
+    v_layer = QgsVectorLayer(data_path, "ROI", "ogr")
+    if not v_layer.isValid():
+        print("Layer failed to load!")
+    else:
+        print("Loaded Vector Layer")
+        QgsProject.instance().addMapLayer(v_layer)
+
+    layout = qgis_load_layout("test/test.qpt")
+
+    # Update title and map extent
+    layout_title = layout.itemById("title")
+    layout_title.setText(title)
+
+    layout_map = layout.itemById("Map 1")
+    layout_map.setExtent(v_layer.extent())
+
+    # Export layout to PDF
+    exporter = QgsLayoutExporter(layout)
+    exporter.exportToPdf(output_path, QgsLayoutExporter.PdfExportSettings())
+    print(f"Exported to {output_path}")
+
+
 def main():
     print("Hello World!")
+
+    qgs = config_qgis()
+    export_map_test("Hello World!", "test/GH_CIS.shp", "test/output.pdf")
+    qgs.exitQgis()
 
 
 if __name__ == "__main__":
